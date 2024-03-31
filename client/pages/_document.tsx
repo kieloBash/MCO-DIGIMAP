464ff55074d47e89cc3a4d3a625736898b996941
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2024-03-31 19:13:49 +0800

    title change

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>GRP6 DIGIMAP - IMAGE STITCHING</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
