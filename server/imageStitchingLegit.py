934e2c4861c66afa4dc1c736dba77723cc7ac444
valen <75712068+Yeonnmi@users.noreply.github.com>
2024-04-04 18:49:21 +0800

    added algorithm

--- server/imageStitchingLegit.py
+++ server/imageStitchingLegit.py
@@ -3,58 +3,89 @@ import cv2
 import glob
 import imutils
 import os
+from matplotlib import pyplot as plt
 
-def stitch_images(files):
-    images = []
+def mix_match(leftImage, warpedImage):
+        i1y, i1x = leftImage.shape[:2]
+        i2y, i2x = warpedImage.shape[:2]
+        for i in range(0, i1x):
+            for j in range(0, i1y):
+                try:
+                    if(np.array_equal(leftImage[j,i],np.array([0,0,0])) and  \
+                        np.array_equal(warpedImage[j,i],np.array([0,0,0]))):
+                        # print "BLACK"
+                        # instead of just putting it with black,
+                        # take average of all nearby values and avg it.
+                        warpedImage[j,i] = [0, 0, 0]
+                    else:
+                        if(np.array_equal(warpedImage[j,i],[0,0,0])):
+                            # print "PIXEL"
+                            warpedImage[j,i] = leftImage[j,i]
+                        else:
+                            if not np.array_equal(leftImage[j,i], [0,0,0]):
+                                bl,gl,rl = leftImage[j,i]
+                                warpedImage[j, i] = [bl,gl,rl]
+                except:
+                    pass
+        # cv2.imshow("waRPED mix", warpedImage)
+        # cv2.waitKey()
+        return warpedImage
 
-    for file in files:
-        # Read the file content as bytes
-        file_bytes = file.read()
-        
-        # Convert the bytes to a NumPy array
-        np_array = np.frombuffer(file_bytes, np.uint8)
-        
-        # Decode the NumPy array to an OpenCV image
-        img = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
-        
-        images.append(img)
-        
-    imageStitcher = cv2.Stitcher_create()
+MIN_MATCH_COUNT = 10
 
-    error, stitched_img = imageStitcher.stitch(images)
+img1 = cv2.imread('./example1.jpg',0)          # queryImage
+img2 = cv2.imread('./example1_2.jpg',0) # trainImage
+im2 = img2.copy()
+# Initiate SIFT detector
+sift = cv2.xfeatures2d.SIFT_create()
 
-    if not error:
-            
-        stitched_img = cv2.copyMakeBorder(stitched_img, 10, 10, 10, 10, cv2.BORDER_CONSTANT, (0,0,0,))
-        gray = cv2.cvtColor(stitched_img, cv2.COLOR_BGR2GRAY)
-        thresh_img = cv2.threshold(gray,0,255, cv2.THRESH_BINARY)[1]
-        
-        
-        contours = cv2.findContours(thresh_img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        
-        contours = imutils.grab_contours(contours)
-        areaOI = max(contours, key=cv2.contourArea)
-        
-        mask = np.zeros(thresh_img.shape, dtype="uint8")
-        x, y, w, h = cv2.boundingRect(areaOI)
-        cv2.rectangle(mask, (x,y), (x + w, y + h), 255, -1)
-        
-        minRectangle = mask.copy()
-        sub = mask.copy()
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
 
-        while cv2.countNonZero(sub) > 0:
-            minRectangle = cv2.erode(minRectangle, None)
-            sub = cv2.subtract(minRectangle, thresh_img)
-            
-        contours = cv2.findContours(minRectangle.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        
-        contours = imutils.grab_contours(contours)
-        areaOI = max(contours, key=cv2.contourArea)
-        
-        x, y, w, h = cv2.boundingRect(areaOI)
-        
-        stitched_img = stitched_img[y:y + h, x:x + w]
-    
-        return stitched_img
-        
-        
+FLANN_INDEX_KDTREE = 0
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+search_params = dict(checks = 50)
+
+flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+matches = flann.knnMatch(des1,des2,k=2)
+print (matches[0])
+print (matches[1])
+# kernel =
+# store all the good matches as per Lowe's ratio test.
+good = []
+for m,n in matches:
+    if m.distance < 0.7*n.distance:
+        good.append(m)
+print(len(good))
+if len(good)>MIN_MATCH_COUNT:
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+    matchesMask = mask.ravel().tolist()
+
+    h,w = img1.shape
+    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+    dst = cv2.perspectiveTransform(pts,M)
+
+    img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+
+else:
+    print ("Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT),
+    matchesMask = None)
+draw_params = dict(matchColor = (0,0,255), # draw matches in green color
+                   singlePointColor = None,
+                   matchesMask = matchesMask, # draw only inliers
+                   flags = 2)
+
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+
+height, width = img1.shape
+im2Reg = cv2.warpPerspective(im2, M, (width, height))
+# stitch = mix_match(img1,im2)
+result = cv2.warpPerspective(im2, M,(im2.shape[1] + img1.shape[1], im2.shape[0]))
+# print(result.shape)
+result[0:img1.shape[0], img1.shape[1]:im2.shape[1]+img1.shape[1]] = im2
+plt.imshow(result , 'gray'),plt.show()
